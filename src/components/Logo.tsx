6a8a78bfee94a593d4f6fdbbc71cb7de49e95223
Josip Hudolin <josip.hudolin315@gmail.com>
2024-02-21 11:47:59 +0100

    izbacio slike

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import "./styles.css";
 const Logo = () => {
   return (
     <div className="top-logo-background">
-      <img className="logo" src="../images/svg/logo-no-background.svg" />
+      milkcheck
     </div>
   );
 };
